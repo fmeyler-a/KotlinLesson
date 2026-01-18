160884511b58c12d9722325a5b427cf02373fbcb
Abusiq <dameyler@gmail.com>
2026-01-18 11:33:53 +0300

    Задача №2

--- src/main/kotlin/Double.kt
+++ src/main/kotlin/Double.kt
@@ -1,6 +1,13 @@
-fun main(){
-    val numbers = listOf(1, 2, 3, 4)
-    val double = numbers.map { it * 2 }
+fun applyToNumbers(numbers: List<Int>, transform: (Int) -> Int): List<Int> {
+    return numbers.map { transform(it) }
+}
 
-    println(double)
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5)
+
+    val doubled = applyToNumbers(numbers) { number ->
+        number * 2
+    } // 2, 4, 6, 8, 10
+
+    println(doubled)
 }
