99098709bc67aeb8dcb9a854cd962d884d4f1ef2
Abusiq <dameyler@gmail.com>
2026-01-18 15:21:18 +0300

    Задача №3

--- src/main/kotlin/TaskThree.kt
+++ src/main/kotlin/TaskThree.kt
@@ -1,14 +1,13 @@
 import kotlin.text.iterator
-
-// Функция печатает только гласные из строки
 fun vowels(s: String): String {
-
     val onlyVawels = "аоуэыяёеюиАОУЭЫЯЕЁЮИ"
+
     for (i in s) {
         if (i in onlyVawels) {
             print(i)
         }
     }
+
     return onlyVawels
 }
 
