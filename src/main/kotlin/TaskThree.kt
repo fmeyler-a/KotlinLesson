45d90bb3152676cc2cff7425d2ca785a55ee1f10
Abusiq <dameyler@gmail.com>
2026-01-19 13:56:04 +0300

    Задача №3

--- src/main/kotlin/TaskThree.kt
+++ src/main/kotlin/TaskThree.kt
@@ -1,16 +1,24 @@
 import kotlin.text.iterator
-fun vowels(s: String): String {
-    val onlyVawels = "аоуэыяёеюиАОУЭЫЯЕЁЮИ"
 
-    for (i in s) {
-        if (i in onlyVawels) {
-            print(i)
+fun String.vowels(): Int {
+    val vowels = listOf('a', 'e', 'i', 'o', 'u', 'y',
+        'A', 'E', 'I', 'O', 'U', 'Y',
+        'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я',
+        'А', 'Е', 'Ё', 'И', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я')
+
+    var count = 0
+
+    for (char in this) {
+        if (char in vowels) {
+            count++
         }
     }
-    return onlyVawels
+
+    return count
 }
 
-fun main() {
+fun main(){
     val text = "Учу КОТЛИН каждый день!"
-    vowels(text)
+    println(text.vowels())
+
 }
