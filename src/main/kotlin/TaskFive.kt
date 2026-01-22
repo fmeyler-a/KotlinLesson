2037b0adf27ee2d1f817d505f0681810a5869c7d
Abusiq <dameyler@gmail.com>
2026-01-22 18:33:01 +0300

    Задача №5

--- src/main/kotlin/TaskFive.kt
+++ src/main/kotlin/TaskFive.kt
@@ -1,11 +1,11 @@
 fun main(){
-    val null1 = mergeStrings("Hello ", "World!")
-    println(null1)
+    val isStringNull = mergeStrings("", "")
+    println(isStringNull)
 }
 
-fun mergeStrings(a: String?, b: String?): String? =
-    if (a != null && b != null) {
-        a + b
-    } else {
-        a ?: b
-    }
+fun mergeStrings(a: String?, b: String?): String? {
+    if (a.isNullOrEmpty() && b.isNullOrEmpty()) return null
+    if (a.isNullOrEmpty()) return b
+    if (b.isNullOrEmpty()) return a
+    return a + b
+}
